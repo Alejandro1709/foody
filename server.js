da80560be9a4804a560d3c1c328e46c76397b025
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-04 12:28:46 -0500

    add morgan loggin for development

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 const path = require('path');
-
 const app = express();
 
+dotenv.config();
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -12,6 +21,8 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
-app.listen('2000', () =>
-  console.log('Server is live at: http://localhost:2000')
+const port = process.env.PORT || 2000;
+
+app.listen(port, () =>
+  console.log(`Server is live at: http://localhost:${port}`)
 );
