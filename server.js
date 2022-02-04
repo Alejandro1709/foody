f57f153840a17cf01b365895331336a03e999585
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-04 18:52:49 -0500

    change some minor layout stuff

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const methodOverride = require('method-override');
 const connectDB = require('./config/connectDB');
+const Restaurant = require('./models/Restaurant');
 const app = express();
 
 dotenv.config();
@@ -24,8 +25,17 @@ app.use('/api/v1/restaurants', require('./routes/restaurant.routes'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-app.get('/', (req, res) => {
-  res.render('restaurants');
+app.get('/', async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find();
+
+    res.render('restaurants', {
+      restaurants,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
 });
 
 // not found route
