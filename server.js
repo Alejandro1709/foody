233782b5426da85602a90d47df4e0a11bf4e1875
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-02-04 12:51:47 -0500

    add get restaurant route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 dotenv.config();
@@ -19,6 +20,23 @@ app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+// routes
+app.get('/api/v1/restaurants', (req, res) => {
+  let data = fs.readFileSync(
+    path.join(__dirname, '/data/restaurants.json'),
+    'utf8'
+  );
+
+  let restaurants = JSON.parse(data);
+
+  res.status(200).json(restaurants);
+});
+
+// not found route
+app.get('*', (req, res) => {
+  res.status(404).json({ message: 'Page Not Found' });
 });
 
 const port = process.env.PORT || 2000;
